629b47e4568859a2d2c196d268c113e7a5bc4d17
N5170 <N5170@CLDV1183.nobilia.org>
2019-05-02 09:16:51 +0200

    tet bv

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-<<<<<<< HEAD
-//test-Tomas
-=======
-		// Test test
->>>>>>> branch 'master' of https://github.com/Jokabu2/CSBMEProjektLagerverwaltung.git
+		// test Vitus
 	}
 
 }
