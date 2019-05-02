8c6f4281e2de489160f62f63e0fa9a194eb3fe4c
N5170 <N5170@CLDV1183.nobilia.org>
2019-05-02 09:56:34 +0200

    IWas

--- src/Frame.java
+++ src/Frame.java
@@ -9,12 +9,58 @@ import javax.swing.*;
 
 public class Frame extends JFrame implements ActionListener{
 
-	J
+	JButton button1;
+    JButton button2;
+    JButton button3;
+    JLabel label;
+    JPanel panel;
 	
 	Frame(){
-		this.setTitle("ActionListener Beispiel");
+		this.setTitle("Lagerverwaltungssystem");
         this.setSize(400, 200);
         panel = new JPanel();
+        
+        //Leeres JLabel-Objekt wird erzeugt
+        label = new JLabel();
+        
+        //Drei Buttons werden erstellt
+        button1 = new JButton("INSERT");
+        button2 = new JButton ("SELECT");
+        button3 = new JButton ("DELETE");
+ 
+        //Buttons werden dem Listener zugeordnet
+        button1.addActionListener(this);
+        button2.addActionListener(this);
+        button3.addActionListener(this);
+ 
+        //Buttons werden dem JPanel hinzugefügt
+        panel.add(button1);
+        panel.add(button2);
+        panel.add(button3);
+ 
+        //JLabel wird dem Panel hinzugefügt
+        panel.add(label);
+        this.add(panel);
+	}
+	
+	
+
+	@Override
+	public void actionPerformed(ActionEvent ae) {
+		// Die Quelle wird mit getSource() abgefragt und mit den
+        // Buttons abgeglichen. Wenn die Quelle des ActionEvents einer
+        // der Buttons ist, wird der Text des JLabels entsprechend geändert
+           
+		if(ae.getSource() == this.button1){
+			label.setText("Button 1 wurde betätigt");
+        }
+        else if(ae.getSource() == this.button2){
+        	label.setText("Button 2 wurde betätigt");
+        }
+        else if (ae.getSource() == this.button3){            
+            label.setText("Button 3 wurde betätigt");
+        }
+		
 	}
 
 }
