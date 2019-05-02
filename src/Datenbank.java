36f97e7114d14809b29bb00ff464f8a220a24fd2
Jokabu2 <49247455+Jokabu2@users.noreply.github.com>
2019-05-02 11:20:42 +0200

    l

--- src/Datenbank.java
+++ src/Datenbank.java
@@ -3,42 +3,62 @@ import java.sql.*;
 import java.util.Calendar;
 
 public class Datenbank {
-
-	Class.forName(myDriver);
-	Connection conn = DriverManager.getConnection(myUrl, "root", "");
-	public static void main(String[] args) {
-		// mySQL Datenbank-Connection erstellen
-		try {
+	// SQL Datum-Objekt erstellen.
+	static java.sql.Date startDate = getStartDate();
 	
-		//SQL Datum-Objekt erstellen.
-			
-			
-			//INSERT-Befehl
+	public static void main(String[] args) {
+		try {
+			// mySQL Datenbank-Connection erstellen
+			String myUrl = getMyUrl();
+			String myDriver = getMyDriver();
+			Class.forName(myDriver);
+			Connection conn = DriverManager.getConnection(getMyUrl(), "root", "");
+	
+			// INSERT-Befehl
 			String query = " insert into TABELLENNAMEN (SPALTENNAMEN)" + " values (?)";
-			
+
 		} catch (Exception e) {
 			System.err.println("In Klasse Datenbank ist ein Fehler aufgetreten!");
-		     System.err.println(e.getMessage());
+			System.err.println(e.getMessage());
 		}
 	}
+
 	public static String getMyUrl() {
 		return "jdbc:mysql://localhost/warenlager?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 	}
-	
+
 	public static String getMyDriver() {
 		return "org.gjt.mm.mysql.Driver";
 	}
-	
+
+	private static Date getStartDate() {
+		Calendar calendar = Calendar.getInstance();
+	    java.sql.Date startDate = new java.sql.Date(calendar.getTime().getTime());
+	    return startDate;
+	}
+
 	public static void insert() {
-		//Vorbereiteter INSERT-Befehl wird verwendet
+		// Vorbereiteter INSERT-Befehl wird verwendet
 		PreparedStatement preInsert = conn.prepareStatement(query);
-		preInsert.setString(1,"Barney");
+		preInsert.setString(1, "Barney");
 		preInsert.setString(2, "Rubble");
 		preInsert.setDate(3, startDate);
-		preInsert.setBoolean(4,false);
-		preInsert.setInt(5,5000);
-		
-		//Vorbereiteten Befehl ausführen
+		preInsert.setBoolean(4, false);
+		preInsert.setInt(5, 5000);
+
+		// Vorbereiteten Befehl ausführen
 		preInsert.execute();
 	}
 }
+
+/*
+DB TABELLE:
+create table users (
+id int unsigned auto_increment not null,
+first_name varchar(32) not null,
+last_name varchar(32) not null,
+date_created timestamp default now(),
+is_admin boolean,
+num_points int,
+primary key (id));
+ */
