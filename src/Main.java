5d2f710526daf60467819d064f0fb0823d6282e0
Jokabu2 <49247455+Jokabu2@users.noreply.github.com>
2019-05-02 09:25:15 +0200

    s

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		// TODO Auto-generated method stub
-<<<<<<< HEAD
-//test-Tomas
-=======
-		// Test test
->>>>>>> branch 'master' of https://github.com/Jokabu2/CSBMEProjektLagerverwaltung.git
-	
-	
-	
-	
-	//test-tomas
-	
-=======
-		// test Vitus
->>>>>>> branch 'master' of https://github.com/Jokabu2/CSBMEProjektLagerverwaltung.git
-	
-	
-	
-	//test
+
 	}
 
 }
