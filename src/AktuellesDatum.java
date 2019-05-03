2e8eaf5bde298ebae020ef97e8051c08e7c3e1ef
Jokabu2 <49247455+Jokabu2@users.noreply.github.com>
2019-05-03 18:31:28 +0200

    p

--- src/AktuellesDatum.java
+++ src/AktuellesDatum.java
@@ -1,11 +1,17 @@
 import java.time.LocalDate;
 
+/**
+ * @category Hilfsklasse
+ */
 public class AktuellesDatum {
 
-	static String getCurrentDate() {
+	/**
+	 * @return Aktuelles Datum wird als String zurückgegeben | Format: Jahr-Monat-Tag
+	 */
+	public static String getCurrentDate() {
 		LocalDate date = LocalDate.now();
 		String d = date.toString();
-		
+
 		return d; // 2016-01-31
 	}
 }
