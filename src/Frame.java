2f7ea9af2b09aaf330e52d9d44d91cfa9149b879
Jokabu2 <49247455+Jokabu2@users.noreply.github.com>
2019-05-03 08:18:23 +0200

    x

--- src/Frame.java
+++ src/Frame.java
@@ -1,3 +1,16 @@
+//Klasse DBConnection
+import java.sql.*;
+import java.util.Calendar;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Vector;
+import java.sql.Date;
+
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -79,5 +92,31 @@ public class Frame extends JFrame implements ActionListener {
 			label.setText("Button 3 wurde betätigt");
 		}
 
+	}
+}
+
+
+class DBConnection{
+
+	static Connection connection;
+
+	public static void main (String [] args) {
+		boolean a = connectToMysql("localhost:3306",  "test",  "root",  "Kunxholli123");
+		System.out.println(a);
+	}
+	
+	public static boolean connectToMysql(String host, String database, String user, String passwd) {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+			String connectionCommand = "jdbc:mysql://"+host+"/"+database+"?user="+user+"&password="+passwd +"&serverTimezone=UTC";
+				 
+			connection = DriverManager.getConnection(connectionCommand);
+			return true;
+
+		} catch (Exception ex) {
+			System.out.println("false");
+			System.err.println(ex);
+			return false;
+		}
 	}
 }
