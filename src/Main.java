666d045a13c205d98ee60c4bb9671bcf5886a0a6
Jokabu2 <49247455+Jokabu2@users.noreply.github.com>
2019-05-03 19:20:39 +0200

    s

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,8 @@
-import javax.swing.*;
 
 public class Main {
 
 	public static void main(String[] args) {
 			
-		Frame fl = new Frame();
-        fl.setVisible(true);
-        fl.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-
 		
 	}
 
